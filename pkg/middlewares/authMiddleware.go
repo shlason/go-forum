13993fa01a87aed8f9441fac59cafe567fbce2c5
Jason <nocvi111@gmail.com>
2022-07-20 21:46:57 +0800

    chore: remove unused statements

--- pkg/middlewares/authMiddleware.go
+++ pkg/middlewares/authMiddleware.go
@@ -1,16 +1,13 @@
 package middlewares
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func Auth() middlewareHandler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("Auth middleware start")
 			h.ServeHTTP(w, r)
-			fmt.Println("Auth middleware end")
 		})
 	}
 }
